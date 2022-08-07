d0a2765ddf37aa5006f663ea02df7bc3013cecbf
Sunil Srivastava <sunilsri.srivastava3@gmail.com>
2022-08-07 09:02:04 +0530

    updated

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,8 @@
 // ref http://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json
 export const API_ENDPOINT_POKEMON_LIST =
-  "http://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json";
+  "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json";
 
 export const API_POKEMON_IMAGE =
-  "http://jherr-pokemon.s3.us-west-1.amazonaws.com";
+  "https://jherr-pokemon.s3.us-west-1.amazonaws.com";
 
 export const MIN_POKEMON_ON_PAGE = 12;
